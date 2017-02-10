ebe2c1fd7f090728621bbe5b1429317117290697
Mark C Allen <mark@markcallen.com>
2017-02-09 20:13:35 -0800

    Changed to use aws container

--- swarm/create_swarm.sh
+++ swarm/create_swarm.sh
@@ -17,21 +17,18 @@ if ! docker history -q mesosphere/aws-cli >/dev/null 2>&1; then
   docker pull mesosphere/aws-cli
 fi
 
-path_to_aws=$(which aws)
-if [ ! -x "$path_to_aws" ] ; then
-   echo "Can't find aws cli, check that its in your path"
-   exit 1;
-fi
-
 : ${AWS_ACCESS_KEY_ID:?"Need to set AWS_ACCESS_KEY_ID"}
 : ${AWS_SECRET_ACCESS_KEY:?"Need to set AWS_SECRET_ACCESS_KEY"}
 : ${AWS_DEFAULT_REGION:?"Need to set AWS_DEFAULT_REGION"}
 : ${AWS_SSH_KEY_ID:?"Need to set AWS_SSH_KEY_ID"}
 : ${AWS_SSH_KEY:?"Need to set AWS_SSH_KEY"}
 
+cp ${AWS_SSH_KEY} .
+AWS_SSH_KEY=/terraform/`basename ${AWS_SSH_KEY}`
+
 AWS="docker run --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION -t $(tty &>/dev/null && echo "-i") mesosphere/aws-cli"
 
-DOCKER_AMI=$($AWS ec2 describe-images --owners self --filters "Name=name,Values=docker*,Name=root-device-type,Values=ebs,Name=virtualization-type,Values=hvm" --query 'Images[*].[ImageId,Name,CreationDate]' --output text | sort -k 3 -r | head -1 | awk '{print $1'})
+DOCKER_AMI=$($AWS ec2 describe-images --owners self --filters "Name=name,Values=docker*" --query 'Images[*].[ImageId,Name,CreationDate]' --output text | sort -k 3 -r | head -1 | awk '{print $1'})
 
 TERRAFORM="docker run --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION -v $PWD:/terraform -i -t hashicorp/terraform:light"
 
@@ -41,3 +38,4 @@ $TERRAFORM $1 -var aws_region=${AWS_DEFAULT_REGION} \
               -var ssh_key_filename=${AWS_SSH_KEY} \
               -state=/terraform/terraform.tfstate /terraform
 
+rm `basename ${AWS_SSH_KEY}`
